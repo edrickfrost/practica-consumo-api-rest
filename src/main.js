e9ba61ab366095923c7fdadabb89837bb9b79f1d
edrickfrost <edrickfrost@gmail.com>
2022-06-11 09:23:14 -0500

    Migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+// Creacion de instancia Axios
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+});
 // funcion para las peliculas en tendencia
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
+
+    const {data} = await api('trending/movie/day');
+
+    // const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    // const data = await res.json();
 
     const movies = data.results;
     //console.log({data, movies})
@@ -23,8 +36,11 @@ async function getTrendingMoviesPreview() {
 
 // funcion para las categorias
 async function getCategoriesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
+
+    const {data} = await api('genre/movie/list')
+
+    // const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    // const data = await res.json();
 
     const categories = data.genres;
     categories.forEach(category => {
