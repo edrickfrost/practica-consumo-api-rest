87704bc7d2220190bfdf0a5302d0a56835622546
edrickfrost <edrickfrost@gmail.com>
2022-06-11 20:54:27 -0500

    creado la funcion navigation.js en el html e implementado la funcion navigator()

---
+++ src/navigation.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator(){
+    console.log({ location });
+
+    if (location.hash.startsWith('#trends')){
+        console.log("Trends!!");
+    } else if (location.hash.startsWith('#search=')){
+        console.log("Search!!");
+    } else if (location.hash.startsWith('#movie=')){
+        console.log("Movie!!");
+    } else if (location.hash.startsWith('#category=')){
+        console.log("Category!!");
+    } else {
+        console.log("Home!!");
+    }
+    location.hash;
+}
